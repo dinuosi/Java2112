7a2bb10736cac39064cffe9618f53e8c057c7d4a
Dinuos <2893416367@qq.com>
2022-02-09 16:36:35 +0800

    my 2

--- src/socket/Server.java
+++ src/socket/Server.java
@@ -1,8 +1,12 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 聊天室服务端
@@ -23,31 +27,45 @@ public class Server {
     public Server(){
         try {
             /*
-                实例化ServerSocket的同时需要指定打开的服务端口，客户端就是通过该端口建立链接的。
-                如果该端口已经被才做系统其他应用程序占用了，那么这里会抛出异常：
+                实例化ServerSocket的同时需要指定打开的服务端口，客户端就是通过该
+                端口建立链接的。
+                如果该端口已经被操作系统其他程序占用了，那么这里会抛出异常:
                 java.net.BindException:address already in use
-                            绑定异常      地址    已经    被占用了
+                         绑定异常        地址    已经     被占用了
              */
             System.out.println("正在启动服务端...");
             server = new ServerSocket(8088);
-            System.out.println("服务端启动完毕！");
+            System.out.println("服务端启动完毕!");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
     public void start(){
         try {
             /*
-                SeverSocket提供的方法：
+                ServerSocket提供的方法:
                 Socket accept()
-                该方法是一个阻塞方法，调用后开始等待，直到一个客户端与服务端建立链接为止，
-                此时该方法会立即返回一个Socket，通过这个Socket与该客户端交互
+                该方法是一个阻塞方法，调用后开始等待，直到一个客户端与服务端建立链接
+                为止，此时该方法会立即返回一个Socket，通过这个Socket与该客户端交互
              */
-            System.out.println("等待客户端连接");
+            System.out.println("等待客户端链接...");
             Socket socket = server.accept();
-            System.out.println("一个客户端连接了！");
+            System.out.println("一个客户端链接了!");
+            /*
+                通过Socket的方法:
+                InputStream getInputStream()
+                获取一个字节输入流，可以读取来自远端计算机发送过来的字节数据
+             */
+            InputStream in = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(in, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line = br.readLine();
+            System.out.println("客户端说"+line);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
