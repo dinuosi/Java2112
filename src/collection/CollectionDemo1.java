499da91780843ce35814c3a88380c500447a89f1
Dinuos <2893416367@qq.com>
2022-02-14 21:40:33 +0800

    my git

--- src/collection/CollectionDemo1.java
+++ src/collection/CollectionDemo1.java
@@ -1,10 +1,13 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * JAVA集合框架
  * 集合和数组一样，可以保存彝族元素，但是集合将元素的操作都封装成了方法，操作简便
  * 并且集合提供了多种不同的实现供我们使用。
- *
+ * <p>
  * java.util.Collection是所有集合的顶级接口，里面定义了所有集合都必须几倍的功能方法
  * 集合有两类常用的子类：
  * java.util.List:可重复的集合，且有序。通常我们称它为“线性表”
@@ -13,5 +16,28 @@ package collection;
  * 两个元素equals比较为true的情况。
  */
 public class CollectionDemo1 {
+    public static void main(String[] args) {
+        //集合只能存放引用类型
+        Collection c = new ArrayList();
+        /*
+            add:加
+            boolean add(E e)
+            向当前集合中添加一个元素，如果该元素成功添加则返回true，否则返回false
+         */
+        c.add("one");
+        c.add("two");
+        c.add("three");
+        c.add("four");
+        c.add("five");
+        c.add("six");
+        System.out.println(c);
+        c.clear();
+
+        int size = c.size();
+        System.out.println("size:"+size);
+        System.out.println(c);
+        boolean isEmpty = c.isEmpty();
+        System.out.println("是否为空集"+isEmpty);
+    }
 
 }
